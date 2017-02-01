a3761426d52b4aca3d271fdc8fa7be0635f1b615
Jowan <jowanwondergem@gmail.com>
2017-02-01 13:12:31 +0100

    homepage circles updates, now work, based upon posts with the right category!

--- wp-content/themes/allaround/home-circles.php
+++ wp-content/themes/allaround/home-circles.php
@@ -60,35 +60,44 @@
 <?php endif;?>
 
 <?php
-// DLOCC If URL is Homepage then do this…
-$homepage = "/";
-$currentpage = $_SERVER["REQUEST_URI"];
-if($homepage==$currentpage):
+if(is_front_page()):
 ?>
 <br><br>
 <div class="clear" data-string="post_type=post&amp;post_status=publish&amp;posts_per_page=4&amp;paged=1&amp;orderby=date&amp;order=ASC&amp;ignore_sticky_posts=0"> 
-<?php $upload_dir = wp_upload_dir(); ?>
+
+
 <?php
-	$i =0;
-	$images = array("/2015/03/FT0A3700", "/2015/03/Blauwe-krukjes", "/2015/03/watercolour-Turkoois1631419_1920","/2016/06/Hele-set");
-	$items =  wp_get_nav_menu_items( 'Hoofdmenu');
-	foreach ($items as $item):
-?>
-<?php if($item->title == "Coaching" || $item->title == "Workshops" || $item->title == "Cursussen" || $item->title == "Producten" ): ?>
 	
+		$postslist = get_posts( array(
+		'category_name' => 'homepage_circles',
+		'post_type'   => 'post',
+  		'post_status' => 'publish',
+	    'numberposts' => 4,
+	    'order'          => 'DESC',
+	    'orderby'        => 'date'
+	) );
+
+
+	foreach ($postslist as $post):
+?>
+<?php
+	$postId = $post->ID;
+	$postTitle = $post->post_title;
+	$postContent = $post ->post_content;
+	$postLink = simple_fields_value('homepage_circle_link', $postId);
+	$postImage = get_the_post_thumbnail_url($post)
+?>
     <div class="column-1-4 column" style="text-align:center; margin-right:0px;" >
-    <a href="<?php echo $item->url ?>">
+    <a href="<?php echo $postLink; ?>">
       <div class="image_wrapper" style="cursor:pointer;">
-         <img width="148" height="148" src="<?php echo $upload_dir['baseurl']; ?><?php echo $images[$i] ?>-148x148.jpg" class="content_image wp-post-image" alt="<?php echo $item->title ?>" srcset="<?php echo $upload_dir['baseurl']; ?><?php echo $images[$i] ?>-148x148.jpg 148w" sizes="(max-width: 148px) 100vw, 148px">
+         <img width="148" height="148" src="<?php echo $postImage; ?>" class="content_image wp-post-image" alt="<?php echo $postTitle; ?>" srcset="<?php echo $postImage; ?> 148w" sizes="(max-width: 148px) 100vw, 148px">
       </div>
-      <!-- image_wrapper -->
       <div class="">
-         <h3 ><?php echo $item->title ?></h3>
+         <h3 ><?php echo $postTitle; ?></h3>
+         <p ><?php echo $postContent; ?></p>
       </div>
       </a>
    </div>
-   <?php $i++ ?>
-<?php endif; ?>
 <?php endforeach; ?>
 
    <!-- blog_post blog2 blog4-->
